3eafbf9088236ee19aa06fb5a1e0adc861ac8fff
Affethepolabe <nils.otto.jonasson@icloud.com>
2024-09-19 14:34:49 +0200

    Update Uppgift1.py

--- Uppgift1.py
+++ Uppgift1.py
@@ -6,7 +6,7 @@ while i <= notal:
     i += 1
 tal.sort()
 summa = sum(tal)
-medel = sum(tal)/len(tal)
+medel = summa/len(tal)
 print(f"summa: {summa}")
 print(f"medel: {medel}")
 print(f"min: {tal[0]}")
@@ -14,5 +14,6 @@ print(f"max: {tal[notal-1]}")
 if len(tal)%2 ==1:
     print(f"median: {tal[len(tal)//2]}")
 else:
-    print(f"median: {float(tal[(len(tal)//2)]+tal[((len(tal)//2)-1)])/2}")
+    print(f"median: {(tal[(len(tal)//2)]+tal[((len(tal)//2)-1)])/2}")
+print(tal)
 
